17daed704f168cc84f4f0fccf2d18cfea22c449c
I-anS <84734686+I-anS@users.noreply.github.com>
2024-06-20 23:13:42 +0800

    Update ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,31 @@
 module.exports = {
   env: {
     browser: true,
-    es6: true,
+    commonjs: true,
+    es2021: true,
     "jest/globals": true,
   },
-  extends: ["eslint:recommended", "plugin:react/recommended"],
+  extends: ["eslint:recommended", "plugin:jest/recommended"],
+  plugins: ["jest"],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parser: "@babel/eslint-parser",
   parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 2018,
-    sourceType: "module",
+    requireConfigFile: false,
   },
-  plugins: ["react", "jest"],
-  rules: {
-    indent: ["error", 2],
-    "linebreak-style": ["error", "unix"],
-    quotes: ["error", "double"],
-    semi: ["error", "never"],
-    eqeqeq: "error",
-    "no-trailing-spaces": "error",
-    "object-curly-spacing": ["error", "always"],
-    "arrow-spacing": ["error", { before: true, after: true }],
-    "no-console": "error",
-    "react/prop-types": 0,
-  },
+  rules: {},
 };
