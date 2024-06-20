15170ebd91fd9177ba04e4f22b843656586e2d53
I-anS <84734686+I-anS@users.noreply.github.com>
2024-06-21 01:44:39 +0800

    Update example.spec.js

--- e2e-tests/example.spec.js
+++ e2e-tests/example.spec.js
@@ -1,24 +1,4 @@
-// // @ts-check
-// const { test, expect } = require('@playwright/test');
-
-// test('has title', async ({ page }) => {
-//   await page.goto('https://playwright.dev/');
-
-//   // Expect a title "to contain" a substring.
-//   await expect(page).toHaveTitle(/Playwright/);
-// });
-
-// test('get started link', async ({ page }) => {
-//   await page.goto('https://playwright.dev/');
-
-//   // Click the get started link.
-//   await page.getByRole('link', { name: 'Get started' }).click();
-
-//   // Expects page to have a heading with the name of Installation.
-//   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-// });
-
-const { test, describe, expect, beforeEach } = require("@playwright/test");
+const { test, describe, expect } = require("@playwright/test");
 
 describe("Pokedex", () => {
   test("front page can be opened", async ({ page }) => {
