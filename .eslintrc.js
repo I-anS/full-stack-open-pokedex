aca949b202de6f77bfe8efe886db4c57444f31dd
I-anS <84734686+I-anS@users.noreply.github.com>
2024-06-20 23:35:27 +0800

    Exercise 11.6 Done

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,46 @@
 module.exports = {
   env: {
-    browser: true,
-    commonjs: true,
-    es2021: true,
-    "jest/globals": true,
+    browser: true, // Enables browser global variables
+    es2021: true, // Enables ES12 features
+    node: true, // Enables Node.js global variables
+    "jest/globals": true, // Enables Jest global variables
   },
-  extends: ["eslint:recommended", "plugin:jest/recommended"],
-  plugins: ["jest"],
+  extends: [
+    "eslint:recommended", // Use recommended configurations
+    "plugin:react/recommended", // Use recommended React settings
+    "plugin:jest/recommended", // Use recommended Jest settings
+  ],
+  parser: "@babel/eslint-parser", // Use Babel ESLint parser for latest syntax support
+  parserOptions: {
+    ecmaVersion: "latest", // Latest ECMAScript standards
+    sourceType: "module", // Support for ES6 modules
+    ecmaFeatures: {
+      jsx: true, // Support for JSX
+    },
+    requireConfigFile: false, // No need for a Babel configuration file
+  },
+  plugins: [
+    "react", // React-specific linting tools
+    "jest", // Jest-specific linting tools
+  ],
+  rules: {
+    "react/react-in-jsx-scope": "off", // Not needed with React 17+
+    "react/jsx-uses-react": "error", // Ensures React is in scope when using JSX
+    "react/jsx-uses-vars": "error", // Prevents unused vars when they are used in JSX
+    // Disable prop-types rule
+    "react/prop-types": "off",
+  },
+  settings: {
+    react: {
+      version: "detect", // Automatically detect the React version
+    },
+  },
   overrides: [
     {
-      env: {
-        node: true,
-      },
-      files: [".eslintrc.{js,cjs}"],
+      files: [".eslintrc.*", "webpack.*.js"], // Specific configurations for ESLint and Webpack config files
       parserOptions: {
-        sourceType: "script",
+        sourceType: "script", // Treat these files as CommonJS (allows require and module.exports)
       },
     },
   ],
-  parser: "@babel/eslint-parser",
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-    ecmaFeatures: {
-      jsx: true,
-    },
-    requireConfigFile: false,
-  },
-  rules: {},
 };
